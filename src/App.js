356a8fd54083cfd642f14d93fc4258fd8a8a71a2
ccherylinn <52254119+ccherylinn@users.noreply.github.com>
2022-04-13 20:31:11 -0500

    fixed css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import { useState, useEffect, Component } from "react";
 import Intake from './Intake.js';
 import Metrics from './Metrics.js';
+import SearchBarPage from './SearchBarPage.js';
+import Page from './Page.js';
+
 
 function App() {
 
   return (
     <div>
-      <Intake />
+      <SearchBarPage />
+      {/* <Page /> */}
     </div>
     
   );
