ec65489f5818bb671628f3e879b645dc45b0dbee
davidyim03 <66441490+davidyim03@users.noreply.github.com>
2022-05-15 17:59:02 -0500

    make it better

--- gnp_react/src/App.js
+++ gnp_react/src/App.js
@@ -6,7 +6,6 @@ import CaseNotes from './CaseNotes.js'
 import ReferralList from './ReferralList.js'
 import AssessmentIntake from './AssessmentIntake.js';
 import HousingPlan from './HousingPlan.js'
-import Intake from './Intake.js';
 import Metrics from './Metrics.js';
 import { Routes, Route, Link } from "react-router-dom";
 import Display from './Display.js';
@@ -16,7 +15,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={< Intake/>} />
+        <Route path="/" element={< CaseNotes />} />
         <Route path="display" element={<Display />} />
         <Route path="metrics" element={<Metrics />} />
       </Routes>
---
+++ gnp_react/src/services/casenotedb.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+axios.defaults.headers.common = {
+  "Content-Type": "application/json"
+}
+
+const baseUrl = 'api/casenote' 
+
+const addCaseNote = activity => {
+    const request = axios.post(baseUrl, activity)
+    
+    return request.then(response => {
+      console.log('this got sent back from the backend', response)
+      return response.data
+    })
+  }
+
+export default {addCaseNote}
