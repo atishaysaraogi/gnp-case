d78f0f6c48b30d90a479d8c96aa624e1ebfdb396
atishaysaraogi <67693777+atishaysaraogi@users.noreply.github.com>
2022-05-25 15:41:38 -0500

    adding data files

--- src/SearchBarPage.js
+++ src/SearchBarPage.js
@@ -2,8 +2,11 @@ import gnp from './GNP_Logo_RGB.png';
 import './App.css';
 import React, {Component} from 'react';
 import { render } from "react-dom";
-import { Button, Search, Dropdown, Grid, Segment} from 'semantic-ui-react';
+import { Button, Search, Dropdown, Grid, Segment, Tab} from 'semantic-ui-react';
 import Bar from './Bar.js';
+import Data from "./data.json"
+import DataSSN from "./data_ssn.json"
+import DataAge from "./data_age.json"
 
 
 const options = [
@@ -12,53 +15,88 @@ const options = [
   {key: 3, value: 3, text: "Claire"}
  ];
 
+ const panes = [
+  { menuItem: 'Search by Name', render: () => <Tab.Pane>
+    <Dropdown
+                button
+                className='icon'
+                fluid
+                labeled
+                icon='search'
+                options={Data}
+                search
+                text='Search'
+              />
+  </Tab.Pane> },
+  { menuItem: 'Search by SSN', render: () => <Tab.Pane>
+    <Dropdown
+                button
+                className='icon'
+                fluid
+                labeled
+                icon='search'
+                options={DataSSN}
+                search
+                text='Search'
+              />
+  </Tab.Pane> },
+  { menuItem: 'Search by Age', render: () => <Tab.Pane>
+    <Dropdown
+                button
+                className='icon'
+                fluid
+                labeled
+                icon='search'
+                options={DataAge}
+                search
+                text='Search'
+              />
+  </Tab.Pane> },
+]
+
 function SearchBarPage() {
   return (
     <div className="App">
-
-        <h1>  </h1> 
-    
-      <h1>Intake Forms</h1>
-
-       <a href='casenotes'><Button size='medium' color='blue' path=''> Case Notes </Button></a>
-       <a href='indserviceplan'><Button size='medium' color='blue' path=''> Individual Service Plan </Button></a>
-       <a href='housingplan'><Button size='medium' color='blue' path=''> Housing Plan </Button></a>
-       <a href='assessmentintake'><Button size='medium' color='blue' path=''> AssessmentIntake </Button></a>
-       <a href='referrallist'><Button size='medium' color='blue' path=''> Referral List </Button></a>
-
-        <h2>  </h2>
-
-        <h2>  </h2>
-
-
-
 
           <Grid columns='equal'>
             <Grid.Column>
             </Grid.Column>
 
-            <Grid.Column width={3}>
+            <Grid.Column width={9}>
               {/* <Search size='big' /> */}
-              <Bar />
+              {/* <Bar /> */}
+
+              {/* <Dropdown
+                button
+                className='icon'
+                fluid
+                labeled
+                icon='search'
+                options={Data}
+                search
+                text='Search'
+              /> */}
+
+              <Tab panes={panes} />
+
+
+
+
+
             </Grid.Column>
 
-            <Grid.Column width={1}>
-              <Dropdown icon='filter' text='sort by'>
+
+            {/* <Grid.Column width={2}>
+              <Dropdown icon='filter' text='search by'>
                 <Dropdown.Menu>
-                <Dropdown.Item text='First Name' description='A - Z' />
-                <Dropdown.Item text='First Name' description='Z - A' />
-                <Dropdown.Item text='Last Name' description='A - Z' />
-                <Dropdown.Item text='Last Name' description='Z - A' />
-                <Dropdown.Item text='Age' description='youngest first' />
-                <Dropdown.Item text='Age' description='oldest first' />
+                <Dropdown.Item text='Name' />
+                <Dropdown.Item text='Age' />
+                <Dropdown.Item text='Intake Period' />
+                <Dropdown.Item text='SSN' description='last 4 digits' />
                 </Dropdown.Menu>
               </Dropdown>
-            </Grid.Column>
+            </Grid.Column> */}
 
-            <Grid.Column width={1}>
-              <Button size='mini'> search </Button>
-              
-            </Grid.Column>
 
             <Grid.Column>
             </Grid.Column>
@@ -66,14 +104,17 @@ function SearchBarPage() {
 
           <h1>  </h1>   
 
-          <Dropdown
+          {/* <Dropdown
             placeholder='Search'
             fluid
             multiple
             search
             selection
             options={options}
-          />
+          /> */}
+
+
+          
           
 
 
