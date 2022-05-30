5e6ca06cd6e6eec38f5fec3a552ca27ed079f6cc
atishaysaraogi <67693777+atishaysaraogi@users.noreply.github.com>
2022-05-30 14:36:10 -0500

    Revert "time dropdown css fixed"
    This reverts commit 24ab86148d5d8dcd9cf2c09acd0d8fc8b4daed04.

--- src/Metrics.js
+++ src/Metrics.js
@@ -163,7 +163,9 @@ function Metrics() {
     <div className="App">
       
       <header className="App-header"> 
+        <div>
           <h1 className = "Summary"> Summary</h1> 
+
           <div className = "timeRange">
               <Dropdown
                 placeholder='Select'
@@ -171,6 +173,9 @@ function Metrics() {
                 selection
                 options={range}/>
           </div>
+  
+          <div id='time'> By Time: </div>
+        </div>
       </header>
       
 
